9dd692f40d1abe299832e7c54b319532fd7a88cb
SeMin <114004065+semsemin@users.noreply.github.com>
2025-03-02 03:21:01 +0900

    fix: #17-1

--- finguide/src/main/java/me/finguide/finguide/S3ClientConfig.java
+++ finguide/src/main/java/me/finguide/finguide/S3ClientConfig.java
@@ -6,12 +6,14 @@ import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 
 @Configuration
 public class S3ClientConfig {
+
     @Value("${cloud.aws.credentials.access-key}")
     private String accessKey;
 
@@ -20,7 +22,7 @@ public class S3ClientConfig {
 
     @Value("${cloud.aws.region.static}")
     private String region;
-  
+
     @Bean
     public S3Client s3Client() {
         AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
