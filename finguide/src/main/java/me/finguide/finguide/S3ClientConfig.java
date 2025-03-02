2ba0be22a038d6e7aca541857343742a77aaa9ee
SeMin <114004065+semsemin@users.noreply.github.com>
2025-03-02 16:26:10 +0900

    fix: #19-재수정 최종

--- finguide/src/main/java/me/finguide/finguide/S3ClientConfig.java
+++ finguide/src/main/java/me/finguide/finguide/S3ClientConfig.java
@@ -29,7 +29,7 @@ public class S3ClientConfig {
 
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(credentials)) 
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
                 .build();
     }
 }
